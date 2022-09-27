a673ff8514b90cd18eec6299b7069611090ace7d
d-sampson <david.sampson@irricheck.co.za>
2022-09-27 17:43:23 +0200

    PU-23a: spinner rework.

--- src/components/common/spinner/Spinner.jsx
+++ src/components/common/spinner/Spinner.jsx
@@ -3,9 +3,12 @@ import { useDispatch } from 'react-redux';
 
 import { bool, string } from 'prop-types';
 
-import { getClassNames } from '../../../tools/general/helpers.util';
-
 import { cancelRequest } from '../../../redux/actions/system.action';
+
+import Graphic from '../graphic/Graphic';
+
+import logo from '../../../tools/images/pulselogo.png';
+import background from '../../../tools/images/irricheckbackground.jpg';
 
 import './spinner.scss';
 
@@ -16,23 +19,39 @@ const Spinner = ({ showSpinnerText, content, centered, sidebar }) => {
   if (!showSpinnerText) return null;
 
   return (
-    <div className="spinner">
-      <div className="spinner__overlay" onClick={ () => dispatch(cancelRequest()) } />
-      <div className={ getClassNames('spinner__container', { content, centered, sidebar }) }>
-        <div className="lds-roller">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+    <div style={ style }>
+      <div>
+        <Graphic loading graphic={ logo } />
+        <div className="spinner">
+          <div className="spinner__overlay" onClick={ () => dispatch(cancelRequest()) } />
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <p className="spinner__text">{ showSpinnerText }</p>
+          </div>
         </div>
-        <div className="spinner__text">{ showSpinnerText }</div>
       </div>
     </div>
   );
+};
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'absolute',
+  height: '150px',
+  width: '250px',
+  marginTop: '-50px',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundImage: `url(${ background })`,
+  boxShadow: '0px 0px 10px black,inset 1px 1px 10px grey',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  zIndex: 20,
+  borderRadius: '5px'
 };
 
 Spinner.propTypes = {
