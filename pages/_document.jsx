f38db28f682441cbb80af99dbb784de9c3ea4273
dsampson94 <davesampson15@gmail.com>
2023-02-09 09:24:19 +0200

    v.0.6 - adds 'Our Brands' screen.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
                             description: 'CAVI Brands is a business motivated by a passion for luxury across Southern Africa. We combine world leading brands with our extraordinary people, our world class expertise, a profound understanding of our local customers, and an intimate knowledge of luxury. We are dedicated to making every customer experience in our stores and with our brands, simply extraordinary.',
                             images: [
                                 {
-                                    url: '/favicon.ico',
+                                    url: '/caviwhitebacklogo.png',
                                     width: 600,
                                     height: 600,
                                     alt: 'CAVI Brands Logo'
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
                     <link rel="shortcut icon" href="../static/favicon.ico" />
                     <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-                    <meta property="og:image" content="/caviwhitebacklogo.png" />
+                    <meta property="og:image" content="/favicon.ico" />
                     {/* Google Maps */ }
                     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2MMmQ2tPm4z0OURwhMNCNBMYlMsDTNu0"></script>
 
